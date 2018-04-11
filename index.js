14a5edd50a95978ac2d394bb2c784bf0b32644c0
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-04-11 06:49:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,18 +11,46 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var newItem = {
+   itemName: item,
+   itemPrice: Math.floor((Math.random() * 100) + 1)
+ };
+ cart.push(newItem);
+return `${item} has been added to your cart.`;
+
+
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length == 0) return "Your shopping cart is empty.";
+  var helpCart = cart.map(function(val){
+    return `${this.itemName} at $${this.itemPrice}`
+  });
+  if (helpCart.length == 1) return `In your cart, you have ${helpCart[0]}.`;
+  else {
+    helpCart[helpCart.length -1] = ` and ${helpCart[helpCart.length -1]}.`;
+    return `In your cart, you have ${helpCart.join()}.`;
+  }
 }
+
 
 function total() {
   // write your code here
+  //
+    var total = cart[itemPrice].reduce(function(a,b){
+      return a+b;
+    },0);
+
+  //}
 }
 
 function removeFromCart(item) {
   // write your code here
+  for(var i=0; i< cart.length; i++){
+    if (cart[i].itemName === item) return cart[i].splice(i,1);
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
